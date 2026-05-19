118f881d8fdbbd1ffc5fb56ba617a21440e28335
Gaurav7173 <gaurav.kumar7173.com>
2026-05-19 18:32:15 +0530

    If else

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,6 +7,20 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
-  protected readonly title = signal('angular-21-tut');
+  islogin = signal(false);
+  show = signal(true);
+  status=signal('failure')
+
+  handlelogin(status: boolean) {
+    this.islogin.set(status);
+  }
+
+  handllestatus(event:Event){
+    let target = event.target as HTMLInputElement;
+    this.status.set(target.value);
+  }
+
+
 }
