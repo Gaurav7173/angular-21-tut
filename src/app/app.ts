253908daf4fa80b941e64319bca08d6887d329b1
Gaurav7173 <gaurav.kumar7173.com>
2026-05-20 07:11:55 +0530

    two way binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -39,5 +40,18 @@ export class App {
 
    }
 
+   name=signal('Angular');
+   age=20;
+
+
+   userdata=signal({name:'John',age:30,email:'john@gmail.com'});
+ 
+   updateuserdata(key:string,val:string){
+    
+    this.userdata.update((item)=>({
+      ...item,[key]:val
+    }))
+    
+   }
 
 }
